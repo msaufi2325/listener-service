578d0b9f9b0b45ed51abf822bd362c5a32914618
Saufi <msaufi2325@gmail.com>
2024-05-06 23:45:39 +0900

    add consumer.go and event.go

---
+++ event/consumer.go
@@ -0,0 +1,32 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel)
+}
---
+++ event/event.go
@@ -0,0 +1,17 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func declareExchange(ch *amqp.Channel) error {
+	return ch.ExchangeDeclare(
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable
+		false,        // auto-deleted?
+		false,        // intrenal?
+		false,        // no-wait?
+		nil,          // arguments?
+	)
+}
